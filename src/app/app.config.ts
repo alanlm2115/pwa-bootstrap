481624cf2feb1a6ffba77d20a824e503723de638
alanlm2115 <alan.mejia@uab.edu.bo>
2025-09-23 01:59:27 -0400

    feat(pwa): enable service worker and web manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,26 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, TitleStrategy } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
-
-import { AppTitleStrategy } from './core/title.strategy';
+import { AppTitleStrategy } from './core/title.strategy'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    
     provideZonelessChangeDetection(),
 
     provideRouter(
       routes,
       withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
     ),
+
+    
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+
+    
     { provide: TitleStrategy, useClass: AppTitleStrategy },
   ],
 };
